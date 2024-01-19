d7bc52e298b3d1a45482c95e13e8ffe47816dab1
IhorBoichuk <ihorboichuk74@gmail.com>
2024-01-19 10:52:27 +0000

    'fix_flake8_storage.py'

--- services.py
+++ services.py
@@ -1,45 +1,55 @@
-from typing import Optional, Dict, Any
+"""
+services.py.
+
+This module provides services.
+"""
+from typing import Any, Dict, Optional
+
 import requests
 
+
 class MyApiClient:
-    """
-    MyApiClient class provides methods for making HTTP requests to a specified API.
-    """
+    """MyApiClient class provides methods for making HTTP requests to API."""
 
     def __init__(self, base_url: str, api_key: str):
         """
-        Initializes an instance of MyApiClient.
+        Initialize an instance of MyApiClient.
 
-        :param base_url: The base URL of the API.
-        :param api_key: The API key for authentication.
+        Parameters:
+            base_url: The base URL of the API.
+            api_key: The API key for authentication.
         """
         self.base_url: str = base_url
         self.api_key: str = api_key
 
-    def make_request(self, endpoint: str, method: str = 'GET', params: Optional[Dict[str, Any]] = None,
-                      data: Optional[Dict[str, Any]] = None) -> requests.Response:
+    def make_request(
+        self,
+        endpoint: str,
+        method: str = 'GET',
+        params: Optional[Dict[str, Any]] = None,
+        data: Optional[Dict[str, Any]] = None,
+    ) -> requests.Response:
         """
-        Makes an HTTP request to the specified endpoint.
+        Make an HTTP request to the specified endpoint.
 
-        :param endpoint: The endpoint to make the request to.
-        :param method: The HTTP method (default is 'GET').
-        :param params: Optional parameters to include in the request.
-        :param data: Optional data to include in the request.
+        Parameters:
+            endpoint: The endpoint to make the request to.
+            method: The HTTP method (default is 'GET').
+            params: Optional parameters to include in the request.
+            data: Optional data to include in the request.
 
-        :return: requests.Response object containing the HTTP response.
+        Returns:
+            requests.Response object containing the HTTP response.
         """
-        url: str = f"{self.base_url}/{endpoint}"
-        
-        # Ensure params is a dictionary to prevent potential issues
+        url: str = f'{self.base_url}/{endpoint}'
         params = params or {}
-        
-        # Add API key to parameters
         params['api_key'] = self.api_key
 
-        # Make the request using getattr and lower to handle various HTTP methods
-        res: requests.Response = getattr(requests, method.lower())(url, params=params, data=data)
+        res: requests.Response = getattr(
+            requests,
+            method.lower(),
+            )(url, params=params, data=data)
 
-        # Raise an exception for HTTP errors
         res.raise_for_status()
 
         return res
--- storage.py
+++ storage.py
@@ -1,42 +1,52 @@
+"""
+storage.py.
+
+This module stores request data.
+"""
 from typing import Any, Optional
 
+
 class ResultStorage:
-    """
+    """ResultStorage class.
+ 
     ResultStorage class provides methods for basic
     CRUD operations on a dictionary-based result storage.
     """
 
     def __init__(self):
-        """
-        Initializes an empty dictionary to store results.
-        """
+        """Initialize an empty dictionary to store results."""
         self.results = {}
 
     def create_result(self, key: str, value: Any):
         """
-        Creates a new result in the storage.
+        Create a new result in the storage.
 
-        :param key: The key for the result.
-        :param value: The value associated with the key.
+        Parameters:
+            key: The key for the result.
+            value: The value associated with the key.
         """
         self.results[key] = value
 
     def read_result(self, key: str) -> Optional[Any]:
         """
-        Reads the result associated with the given key.
+        Read the result associated with the given key.
 
-        :param key: The key for the result.
+        Parameters:
+            key: The key for the result.
 
-        :return: The value associated with the key, or None if the key does not exist.
+        Returns:
+            The value associated with the key,
+            or None if the key does not exist.
         """
         return self.results.get(key)
 
     def update_result(self, key: str, new_value: Any):
         """
-        Updates the result associated with the given key.
+        Update the result associated with the given key.
 
-        :param key: The key for the result.
-        :param new_value: The new value to be associated with the key.
+        Parameters:
+            key: The key for the result.
+            new_value: The new value to be associated with the key.
         """
         if key in self.results:
             self.results[key] = new_value
@@ -45,9 +55,10 @@ class ResultStorage:
 
     def delete_result(self, key: str):
         """
-        Deletes the result associated with the given key.
+        Delete the result associated with the given key.
 
-        :param key: The key for the result.
+        Parameters:
+            key: The key for the result.
         """
         if key in self.results:
             del self.results[key]
